755ddb3f482a86715537af9ef190055a194c4929
Nicky West <nickwest@uw.edu>
2021-12-13 12:43:00 -0800

    Fixed a bug where size was used instead of strlen

--- src/NetIDGroups.php
+++ src/NetIDGroups.php
@@ -4,42 +4,42 @@ namespace Drupal\uw_groups;
 use Drupal\lts\Person;
 
 class NetIDGroups{
-	
+
 	private $Person;
 
 	public function __construct(){
 		$this->Person = new Person;
 	}
-	
+
 	public function getCurrentUserGroups(){
 		return $this->Person->GetGroups(\Drupal::currentUser()->getAccountName());
 	}
-	
+
 	public function getGroupsByNetID($netid){
 		return $this->Person->GetGroups($netid);
 	}
-	
+
 	public function getGroupMembers($group){
 		return $this->Person->GetGroupMembers($group);
 	}
-	
+
 	public function getActiveGroups(){
-		if(sizeof(trim(\Drupal::config('uw_groups.settings')->get('active_groups'))) == 0){
+		if(strlen(trim(\Drupal::config('uw_groups.settings')->get('active_groups'))) == 0){
 			return array();
 		}
-		
+
 		$active_groups = explode("\n", \Drupal::config('uw_groups.settings')->get('active_groups'));
 		foreach($active_groups as $key => $active_group){
 			$active_groups[$key] = trim($active_group);
 		}
-		
+
 		return $active_groups;
 	}
-	
+
 	public function isNetIDInAnyActiveGroup($netid){
 		$active_groups = $this->getActiveGroups();
 		$groups = $this->getGroupsByNetID($netid);
-		
+
 		foreach($groups as $group){
 			foreach($active_groups as $active_group){
 				if(trim($group) == trim($active_group) && trim($group) != ''){
@@ -47,7 +47,7 @@ class NetIDGroups{
 				}
 			}
 		}
-		
+
 		return false;
 	}
 }
